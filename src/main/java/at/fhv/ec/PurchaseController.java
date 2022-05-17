4491d10fa2fc9246f1cd5dc70f99e8ca8b5cdd4f
Smightym8 <michaelspiegel@live.at>
2022-05-17 18:12:36 +0200

    Worked on connection refused error and added openapi

--- src/main/java/at/fhv/ec/PurchaseController.java
+++ src/main/java/at/fhv/ec/PurchaseController.java
@@ -1,8 +1,11 @@
 package at.fhv.ec;
 
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -11,8 +14,9 @@ public class PurchaseController {
 
     // TODO: Change to appropriate consumes etc.
     @POST
-    @Consumes(MediaType.WILDCARD)
-    public Response receivePurchase() {
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response receivePurchase(@RequestBody String something) {
         // TODO: Implement logic after receiving new purchase of digital product
         System.out.println("Received new purchase");
 

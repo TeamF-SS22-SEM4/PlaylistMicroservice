e95c875219272ef4254956ad7f6080c2828ed26f
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-05-22 14:26:41 +0200

    run redis in k8-cluster

--- run-k8-local.ps1
+++ run-k8-local.ps1
@@ -1,8 +1,18 @@
+# redis stuff
+kubectl delete -f ./kubernetes/deploy/redis-queue-config.yml
+kubectl delete -f ./kubernetes/deploy/redis-queue.yml
+kubectl delete service queue-service
+
+kubectl apply -f ./kubernetes/deploy/redis-queue-config.yml
+kubectl apply -f ./kubernetes/deploy/redis-queue.yml
+kubectl expose pod redis-queue --type=LoadBalancer --name=queue-service
+
+
 kubectl delete deployments playlist-app
 kubectl delete service playlist-service
 ./gradlew build
 docker build -f src/main/docker/Dockerfile.jvm -t team-f-playlist-microservice-jvm .
-kubectl apply -f ./test-deployment.yml
+kubectl apply -f ./kubernetes/deploy/test-deployment.yml
 # TODO figure out what types are available here and which is best
 kubectl expose deployment playlist-app --type=LoadBalancer --name=playlist-service
 
--- src/main/java/at/fhv/ec/communication/EventListener.java
+++ src/main/java/at/fhv/ec/communication/EventListener.java
@@ -20,25 +20,21 @@ public class EventListener {
 
 
     void onStart(@Observes StartupEvent startupEvent) {
-        try {
-            //TODO get from env, also currently requires the redis instance to be running
-            JedisPool jedisPool = new JedisPool("localhost", 6379);
+        //TODO get from env, also currently requires the redis instance to be running
+        JedisPool jedisPool = new JedisPool("queue-service", 6379);
 
-            try (Jedis jedis = jedisPool.getResource()) {
-                Thread blockingReceiver = new Thread(() -> {
-                    while (true) {
-                        List<String> events = jedis.brpop(0, PURCHASE_EVENT_QUEUE_NAME);
-                        //first result from brpop is the Key of the list -> discard that
-                        for (int i = 1; i < events.size(); i += 1) {
-                            System.out.println("received"  + events.get(i));
-                            //TODO parse from json (move event to shared lib then use gson) and send to application service
-                        }
+        try (Jedis jedis = jedisPool.getResource()) {
+            Thread blockingReceiver = new Thread(() -> {
+                while (true) {
+                    List<String> events = jedis.brpop(0, PURCHASE_EVENT_QUEUE_NAME);
+                    //first result from brpop is the Key of the list -> discard that
+                    for (int i = 1; i < events.size(); i += 1) {
+                        System.out.println("received"  + events.get(i));
+                        //TODO parse from json (move event to shared lib then use gson) and send to application service
                     }
-                });
-                blockingReceiver.start();
-            }
-        } catch (Exception e) {
-            //TODO remove this try/catch, once redis is also in kluster
+                }
+            });
+            blockingReceiver.start();
         }
     }
 }

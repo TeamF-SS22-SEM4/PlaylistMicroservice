5298fb6478ef6401388c8c91604f64572c37f15e
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-05-24 22:13:10 +0200

    stuff

--- src/main/java/at/fhv/ec/communication/rest/PlaylistController.java
+++ src/main/java/at/fhv/ec/communication/rest/PlaylistController.java
@@ -15,6 +15,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.UUID;
 
 @Path("/playlists")
 public class PlaylistController {

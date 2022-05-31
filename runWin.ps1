65e8474c7b20f096ba330aa3303296ddbdbb49a2
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-05-31 09:50:02 +0200

    removed compose and duplicate mp3

--- runWin.ps1
+++
@@ -1,7 +0,0 @@
-docker-compose down
-docker-compose -f infrastructure/redis.yml down
-
-docker-compose -f infrastructure/redis.yml up -d
-./gradlew quarkusBuild
-docker-compose build
-docker-compose up -d

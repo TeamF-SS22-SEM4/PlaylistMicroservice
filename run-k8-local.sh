f950cdf52c4efdf21aeadef710b8a3ccd6751449
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-06-01 15:20:49 +0200

    Revert "removed exposed service"
    This reverts commit e21cf4eae01ced18dc174ef1dc67b466d040c1c5.

--- run-k8-local.sh
+++ run-k8-local.sh
@@ -2,12 +2,14 @@ eval $(minikube docker-env)
 
 kubectl delete -f ./kubernetes/deploy/redis-queue-config.yml
 kubectl delete -f ./kubernetes/deploy/redis-queue.yml
+kubectl delete service queue-service
 
 kubectl delete deployment,svc postgresql
 # kubectl delete pvc postgresql-pv-claim
 # kubectl delete pv postgresql-pv-volume
 
 kubectl delete deployments playlist-app
+kubectl delete service playlist-service
 
 kubectl delete -f ./kubernetes/deploy/secret.yml
 
@@ -16,6 +18,7 @@ kubectl apply -f ./kubernetes/deploy/secret.yml
 # redis stuff
 kubectl apply -f ./kubernetes/deploy/redis-queue-config.yml
 kubectl apply -f ./kubernetes/deploy/redis-queue.yml
+kubectl expose pod redis-queue --type=LoadBalancer --name=queue-service
 
 # postgres
 # kubectl apply -f ./kubernetes/deploy/postgres-persistent-volume.yml
@@ -24,3 +27,11 @@ kubectl apply -f ./kubernetes/deploy/postgres-deployment.yml
 ./gradlew build
 docker build -f src/main/docker/Dockerfile.jvm -t team-f-playlist-microservice-jvm .
 kubectl apply -f ./kubernetes/deploy/playlist-deployment.yml
+# TODO figure out what types are available here and which is best
+kubectl expose deployment playlist-app --type=LoadBalancer --name=playlist-service
+
+# optional, displays information after the cluster has been created
+# kubectl describe deployment playlist-app
+# kubectl describe service playlist-service
+
+# after this script a hello world should be available at localhost:8080/purchase

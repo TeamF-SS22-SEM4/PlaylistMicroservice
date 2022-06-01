e21cf4eae01ced18dc174ef1dc67b466d040c1c5
Lukas Kaufmann <lukas.kaufm@outlook.com>
2022-06-01 14:54:21 +0200

    removed exposed service

--- run-k8-local.ps1
+++ run-k8-local.ps1
@@ -1,13 +1,11 @@
 kubectl delete -f ./kubernetes/deploy/redis-queue-config.yml
 kubectl delete -f ./kubernetes/deploy/redis-queue.yml
-kubectl delete service queue-service
 
 kubectl delete deployment,svc postgresql
 kubectl delete pvc postgresql-pv-claim
 kubectl delete pv postgresql-pv-volume
 
 kubectl delete deployments playlist-app
-kubectl delete service playlist-service
 
 kubectl delete -f .\kubernetes\deploy\secret.yml
 
@@ -16,7 +14,6 @@ kubectl apply -f .\kubernetes\deploy\secret.yml
 # redis stuff
 kubectl apply -f ./kubernetes/deploy/redis-queue-config.yml
 kubectl apply -f ./kubernetes/deploy/redis-queue.yml
-kubectl expose pod redis-queue --type=LoadBalancer --name=queue-service
 
 # postgres
 kubectl apply -f ./kubernetes/deploy/postgres-persistent-volume.yml
@@ -25,11 +22,3 @@ kubectl apply -f ./kubernetes/deploy/postgres-deployment.yml
 ./gradlew build
 docker build -f src/main/docker/Dockerfile.jvm -t team-f-playlist-microservice-jvm .
 kubectl apply -f ./kubernetes/deploy/playlist-deployment.yml
-# TODO figure out what types are available here and which is best
-kubectl expose deployment playlist-app --type=LoadBalancer --name=playlist-service
-
-# optional, displays information after the cluster has been created
-# kubectl describe deployment playlist-app
-# kubectl describe service playlist-service
-
-# after this script a hello world should be available at localhost:8080/purchase

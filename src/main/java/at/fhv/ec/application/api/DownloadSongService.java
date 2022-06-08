f9afebd8b6be4f2e2c452ec5d8a28e32c93ff6c6
Smightym8 <michaelspiegel@live.at>
2022-06-08 16:02:01 +0200

    Removed Download

--- src/main/java/at/fhv/ec/application/api/DownloadSongService.java
+++
@@ -1,9 +0,0 @@
-package at.fhv.ec.application.api;
-
-import java.io.IOException;
-import java.util.NoSuchElementException;
-import java.util.UUID;
-
-public interface DownloadSongService {
-    byte[] downloadSong(UUID songId) throws NoSuchElementException, IOException;
-}

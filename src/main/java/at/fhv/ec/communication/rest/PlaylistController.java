46c93240025527de4a7a8e8cec77e6a8eed39e32
Smightym8 <michaelspiegel@live.at>
2022-06-17 21:38:37 +0200

    Removed session check

--- src/main/java/at/fhv/ec/communication/rest/PlaylistController.java
+++ src/main/java/at/fhv/ec/communication/rest/PlaylistController.java
@@ -7,9 +7,10 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponseSchema;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
@@ -30,22 +31,7 @@ public class PlaylistController {
     @APIResponse(responseCode = "401", description = "Unauthorized")
     @APIResponseSchema(value = PlayableSongDTO[].class, responseCode = "200")
     @Operation(operationId = "getPlaylist", summary = "Get the Playlist of user")
-    public Response getPlaylist(@PathParam("username") String username, @HeaderParam("session-id") String sessionId) {
-        /*
-        TODO: Remove or change it so it works deployed
-        Client client = ClientBuilder.newClient();
-        try {
-            client.target("http://musicshop-backend:8080/api/v1/validateSession?sessionId=" + sessionId)
-                    .request(MediaType.TEXT_PLAIN)
-                    .get(String.class);
-        } catch (WebApplicationException e) {
-            if (e.getResponse().getStatus() == Response.Status.NOT_FOUND.getStatusCode()) {
-                return Response.status(Response.Status.FORBIDDEN).build();
-            }
-        }
-         */
-
-
+    public Response getPlaylist(@PathParam("username") String username) {
         try {
             List<PlayableSongDTO> songList = playlistService.playlistByUsername(username);
             return Response.ok().entity(songList).build();
